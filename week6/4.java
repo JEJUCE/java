16e66193c635f4c3c72a9d8367f0b9324a48e0ea
JejuCjm <cjm6794@gmail.com>
2022-10-20 09:43:59 +0900

    10.20

---
+++ week6/4.java
@@ -0,0 +1,18 @@
+public class 4  {
+	public static void main(String[] args) { 
+		
+		int[] array = {1,2,3,4,5,6,7,8,9};
+		
+		System.out.printf("%s%8s%n", "Index", "Value");
+		
+		
+		for(int counter =0; counter<array.length;counter++) {
+			System.out.printf("%5d%8d%n", counter, array[counter]);
+		}
+		
+		
+		
+		
+		
+	}
+}
