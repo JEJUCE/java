7c89e3e1ee6cfaf3b3861378030ad76d792bc0fe
JejuCjm <cjm6794@gmail.com>
2022-12-13 21:16:31 +0900

    12.13

---
+++ week14/TotalNumbers.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TotalNumbers {
+   public static void main(String[] args) {
+ 
+      Number[] numbers = {1, 2.4, 3, 4.1}; 
+      List<Number> numberList = new ArrayList<>();           
+
+      for (Number element : numbers) {
+         numberList.add(element); // place each number in numberList
+      }
+
+      System.out.printf("numberList contains: %s%n", numberList);
+      System.out.printf("Total of the elements in numberList: %.1f%n", 
+         sum(numberList));
+   }
+
+ 
+   public static double sum(List<Number> list) {
+      double total = 0; // initialize total
+
+  
+      for (Number element : list) {     
+         total += element.doubleValue();
+      }                                 
+
+      return total;
+   }
+}
+
